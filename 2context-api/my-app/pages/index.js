7ccd5c0002c85a2f9b2b4c3b5e8ac4df2840538f
gkurkjian <gkurkjian@myseneca.ca>
2023-05-25 19:28:34 -0400

    header added in context-api to clarify when come back to check later

--- 2context-api/my-app/pages/index.js
+++ 2context-api/my-app/pages/index.js
@@ -5,6 +5,8 @@ import ComponentA from '../components/ComponentA'
 export default function Home(props) {
   return (
     <>
+      <h1>This is from context-api Practice Folder</h1>
+      <h3>Here on each clicking the value, will render the all components. Do inspect and see the messages</h3>
       <Component1 count={props.count}/> <br />
       <ComponentA setCount={props.setCount}/>
     </>
