4c920df827ad16a3ce84a920da517f1043c0dee6
gkurkjian <gkurkjian@myseneca.ca>
2023-08-14 18:08:56 -0400

    Confusion name of the folder. Fixed now

--- 2context-api/my-app/pages/index.js
+++ 2context-api/my-app/pages/index.js
@@ -5,8 +5,11 @@ import ComponentA from '../components/ComponentA'
 export default function Home(props) {
   return (
     <>
-      <h1>This is from context-api Practice Folder</h1>
-      <h3>Here on each clicking the value, will render the all components. Do inspect and see the messages</h3>
+    <h2>We implemented like an Hierarchy <i>count</i> & <i>setCount</i> in pages/_app.js</h2>
+    <h3>It's passing from Component1/Component2/Component3 to use the count by using React.countContext. <br />
+    This goes the same on ComponentA to C applying React.countContext by using setCount. <br />
+    For more info, please open up the _app.js and take a look.
+    </h3> <br />
       <Component1 count={props.count}/> <br />
       <ComponentA setCount={props.setCount}/>
     </>
