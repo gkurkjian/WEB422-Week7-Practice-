9d38603a9cab7dd75294cc0a0a8b1fc1ba6cdd72
gkurkjian <gkurkjian@myseneca.ca>
2023-08-14 17:19:03 -0400

    comment added to explain better later on

--- 1prop-driling/my-app/pages/index.js
+++ 1prop-driling/my-app/pages/index.js
@@ -5,6 +5,11 @@ import ComponentA from '../components/ComponentA'
 export default function Home(props) {
   return (
     <>
+    <h2>We implemented like an Hierarchy <i>count</i> & <i>setCount</i> in pages/_app.js</h2>
+    <h3>It's passing from Component1/Component2/Component3 to use the count by using React.countContext. <br />
+    This goes the same on ComponentA to C applying React.countContext by using setCount. <br />
+    For more info, please open up the _app.js and take a look.
+    </h3> <br />
       <Component1 count={props.count}/> <br />
       <ComponentA setCount={props.setCount}/>
     </>
